bf8a64dc3fa4b54a16fa1b86efef0b80d5fa5c1f
Daniela Lombana (CO) <daniela.lombana@bcdtravel.com.co>
2020-11-26 21:29:56 -0500

    Subo cambios Daniela Fuquene.

---
+++ src/app/Components/examen-oferta/examen-oferta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-examen-oferta',
+  templateUrl: './examen-oferta.component.html',
+  styleUrls: ['./examen-oferta.component.css']
+})
+export class ExamenOfertaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
